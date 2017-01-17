981548b459db286d9c4925b62cd9a88455e48ea7
ReiiYuki <iguru.shou.5410070@gmail.com>
2017-01-18 00:13:17 +0700

    Dummy Data

---
+++ data.js
@@ -0,0 +1,18 @@
+const user = [
+  {
+    "username" : "ReiiYuki",
+    "password" : "abcd",
+    "email" : "iguru.shou.5410070@gmail.com",
+    "level" : 0,
+    "is_active" : true
+  },
+  {
+    "username" : "voraton.l",
+    "password" : "1111",
+    "email" : "voraton.l@ku.th",
+    "level" : 1,
+    "is_active" : true
+  }
+]
+
+export user
